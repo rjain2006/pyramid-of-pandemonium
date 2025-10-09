6e04c307732d74d5f39998c0920afe050d1a64c8
Rishika Jain <jrishika218@gmail.com>
2025-10-09 09:57:23 +0530

    Added Pages and Connect routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
-import {Routes,Route} from 'react-router-dom'
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Onboarding from './components/Onboarding';
+import AlgorithmSelector from './components/AlgorithmSelector';
+import Visualization from './components/Visualization';
+import PerformanceDashboard from './components/PerformanceDashboard';
+import QuizMode from './components/QuizMode';
 
 const App = () => {
   return (
-    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
-      <Navbar/>
-       <Routes>
-          
-       </Routes>
+    <Router>
+      <div className="min-h-screen bg-gradient-to-b from-yellow-100 via-orange-100 to-yellow-200 text-gray-900">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Onboarding />} />
+          <Route path="/select-algorithm" element={<AlgorithmSelector />} />
+          <Route path="/visualize" element={<Visualization />} />
+          <Route path="/dashboard" element={<PerformanceDashboard />} />
+          <Route path="/quiz" element={<QuizMode />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
-    </div>
-  )
-}
-
-export default App
+export default App;
