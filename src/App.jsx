608c8f8b5a3fca5cadc882b0c876d7cc96f5246f
Rishika Jain <jrishika218@gmail.com>
2025-10-09 10:48:38 +0530

    Bug fixing

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,8 @@ const App = () => {
       <div className="min-h-screen bg-gradient-to-b from-yellow-100 via-orange-100 to-yellow-200 text-gray-900">
         <Header />
         <Routes>
-          <Route path="/" element={<Onboarding />} />
+          <Route path="/" element={<Header/>} />
+          <Route path="/onboarding" element={<Onboarding/>} />
           <Route path="/select-algorithm" element={<AlgorithmSelector />} />
           <Route path="/visualize" element={<Visualization />} />
           <Route path="/dashboard" element={<PerformanceDashboard />} />
