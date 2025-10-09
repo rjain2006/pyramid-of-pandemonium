7635b8b4886b1a26fb498febe63580d075ced78c
Rishika Jain <jrishika218@gmail.com>
2025-10-09 11:17:18 +0530

    Fixing Bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+
+import Header from './pages/Header';
+import Visualization from './pages/Visualization';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/visualization" element={<Visualization />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
